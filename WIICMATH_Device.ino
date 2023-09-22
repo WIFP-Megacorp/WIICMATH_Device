c97991b410ce6d017909065d0f25c118a04c4491
idiot <32636970+ostepizza@users.noreply.github.com>
2023-09-22 03:10:55 +0200

    Cleanup
    Converted modes from if-statements to methods
    Added settings for modules
    Attempted to clean up variable names
    Removed need for status-variables

--- WIICMATH_Device.ino
+++ WIICMATH_Device.ino
@@ -10,13 +10,22 @@ const uint8_t PIN_DHT = 2;
 
 unsigned long time_now = 0;
 
-bool statusActive = 0;
-bool statusConnecting = 0;
-bool statusAlarm = 0;
+bool firstSetup = false; //First time launching device?
+bool connected = true; //TBD: if connected to mr internet
 
-float temperature;
-float humidity;
-int updateDelay = 1000;
+float dht_temperature;
+float dht_humidity;
+
+//some settings
+bool MODULE_BUZZER = true; //Is the Buzzer module present?
+bool MODULE_RGBLED = true; //Is the RGB LED module present?
+bool setting_sound = false; //If true, buzzer makes sound
+bool setting_light = true; //If true, RGB LED is used for status
+int setting_minTemp = 10;
+int setting_maxTemp = 27;
+int setting_minHum = 30;
+int setting_maxHum = 70;
+int setting_updateDelay = 1000;
 
 DHT dht(PIN_DHT, DHTTYPE);
 
@@ -33,55 +42,43 @@ void setup() {
 }
 
 void loop() {
-  humidity = dht.readHumidity();
-  temperature = dht.readTemperature(); 
+  dht_humidity = dht.readHumidity();
+  dht_temperature = dht.readTemperature(); 
   time_now = millis();
 
-  if (isnan(humidity) || isnan(temperature)) {
-    statusAlarm = 1;
-    Serial.println(F("Failed to read from DHT sensor!"));
-    Serial.println(humidity);
-    return;
+  //If first time launching device, run setup
+  if(firstSetup) {
+    modeSetup();
+  }
+  
+  readSensor();
+
+  if(!connected) { //If not connected to internet
+    modeConnecting();
+  } else if(dht_temperature < setting_minTemp || dht_temperature > setting_maxTemp  || dht_humidity < setting_minHum || dht_humidity > setting_maxHum) { //If temp & humidity not within spec
+    modeAlarm();
   } else {
-    statusActive = 1;
-    Serial.println("Temp: " + String(temperature, 2) + "°C");
-    Serial.println("Humidity: " + String(humidity, 1) + "%");
-    Serial.println("---");
+    modeNormal();
   }
 
-  if(temperature < 10 || temperature > 27  || humidity < 10 || humidity > 70) { 
-    statusAlarm = 1;
-    statusActive = 0;
-  } else {
-    statusAlarm = 0;
-    statusActive = 1;
-  }
+  millisDelay(setting_updateDelay);
+}
 
-  if (statusAlarm) {
-    Serial.println("It's panic time");
-    statusActive = 0;
-    statusConnecting = 0;
-    ledSignal(1, 1, 0, 0, 500);
-    tone(PIN_BUZZER, 2093, 1000);
-    millisDelay(1000);
-    tone(PIN_BUZZER, 4186, 1000);
-    millisDelay(1000);
-    noTone(PIN_BUZZER);
-  }  
-
-  if (statusActive) {
-    Serial.println("Normal operating");
-    statusConnecting = 0;
-    statusAlarm = 0;
+//Modes
+void modeNormal() {
+  Serial.println("Normal operating");
+  if(MODULE_RGBLED && setting_light) {
     ledSignal(1, 0, 1, 0, 0);
     millisDelay(1000);
   }
-  
-  if (statusConnecting) {
-    Serial.println("Attempting to connect to server");
-    statusActive = 0;
-    statusAlarm = 0;
+}
+
+void modeConnecting() {
+  Serial.println("Attempting to connect to server");
+  if(MODULE_RGBLED && setting_light) {
     ledSignal(1, 0, 0, 1, 1000);
+  }
+  if(MODULE_BUZZER && setting_sound) {
     tone(PIN_BUZZER, 523, 250);
     millisDelay(250);
     tone(PIN_BUZZER, 1047, 250);
@@ -90,10 +87,27 @@ void loop() {
     millisDelay(500);
     noTone(PIN_BUZZER);
   }
-
-  millisDelay(updateDelay);
 }
 
+void modeSetup() {
+  //do some things if first launch
+}
+
+void modeAlarm() {
+  Serial.println("It's panic time");
+  if(MODULE_RGBLED && setting_light) {
+    ledSignal(1, 1, 0, 0, 500);
+  }
+  if(MODULE_BUZZER && setting_sound) {
+    tone(PIN_BUZZER, 2093, 1000);
+    millisDelay(1000);
+    tone(PIN_BUZZER, 4186, 1000);
+    millisDelay(1000);
+    noTone(PIN_BUZZER);
+  }
+}
+
+//LED Handling
 void ledSignal(bool active, bool r, bool g, bool b, int delayMs) {
   turnOffAllLed();
   digitalWrite(11, r);
@@ -112,8 +126,30 @@ void turnOffAllLed() {
   digitalWrite(13, 0);
 }
 
+//Other
 void setUpdateDelay(int ms) {
-  updateDelay = ms;
+  setting_updateDelay = ms;
+}
+
+void readSensor() {
+  if (isnan(dht_humidity) || isnan(dht_temperature)) {
+    modeAlarm();
+    Serial.println("---");
+    Serial.println(F("Failed to read from DHT sensor!"));
+    
+    return;
+  } else {
+    Serial.println("---");
+    Serial.println("Temp: " + String(dht_temperature, 2) + "°C");
+    Serial.println("Humidity: " + String(dht_humidity, 1) + "%");
+  }
+}
+
+void setLimits(int minTemperature, int maxTemperature, int minHumidity, int maxHumidity) {
+  setting_minTemp = minTemperature;
+  setting_maxTemp = maxTemperature;
+  setting_minHum = minHumidity;
+  setting_maxHum = maxHumidity;
 }
 
 void millisDelay(long int delayTime){
