62d12a0f1f92f755eb20693759ac0ca4f5c497fd
idiot <32636970+ostepizza@users.noreply.github.com>
2023-09-27 18:11:49 +0200

    Small change to serial monitor print

--- WIICMATH_Device.ino
+++ WIICMATH_Device.ino
@@ -265,17 +265,14 @@ void millisDelay(long int delayTime){
 void printWiFiStatus() {
   // print the SSID of the network you're attached to:
   Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
+  Serial.print(WiFi.SSID());
 
   // print your WiFi shield's IP address:
   IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
+  Serial.print(" | IP Address: ");
+  Serial.print(ip);
 
-  // print where to go in a browser:
-  Serial.print("To see this page in action, open a browser to http://");
-  Serial.println(ip);
-
+  Serial.println("");
 }
 
 void handleConfigure() {
